a3e21d6d92a05d1bb1cde600abac89142451b73e
dkilfoyle <dkilfoyle@xtra.co.nz>
2017-01-15 09:09:59 +1300

    blue background

--- raytraceR.R
+++ raytraceR.R
@@ -29,21 +29,30 @@ bmp.plot = function(bmp) {
     print(p)
 }
 
-createRay = function(origin = c(0,0,0), direction = c(1,0,0))
-{
+createRay = function(origin = c(0,0,0), direction = c(1,0,0)) {
   return(list(
     origin=origin,
     direction=direction
   ))
 }
 
+ray.length = function(ray) {
+  return(sqrt(ray$direction %*% ray$direction))
+}
+
 hit.sphere = function(center, radius, ray) {
   oc = ray$origin - center
-  a = ray$direction %*% ray$direction
+  a = ray$direction %*% ray$direction # x^2 + y^2 + z^2
   b= 2.0 * (oc %*% ray$direction)
   c = (oc %*% oc) - radius * radius
   discriminant = b * b - 4 * a* c
   return(discriminant>0)
+}
+
+getBackgroundColor = function(ray) {
+  unit.direction = ray$direction / ray.length(ray)
+  t = 0.5 * (unit.direction[2] + 1.0)
+  return( (1.0-t) * c(1,1,1) + (t*c(0.5,0.7,1.0)) )
 }
 
 tst = function() {
@@ -60,6 +69,8 @@ tst = function() {
       if (hit.sphere(center=c(0,0,-1), radius=0.5, ray)) {
         bmp$pixels[(j-1)*bmp$width+i,]=c(1,0,0)
       }
+      else
+        bmp$pixels[(j-1)*bmp$width+i,]=getBackgroundColor(ray)
     }
   }
   bmp.plot(bmp)
