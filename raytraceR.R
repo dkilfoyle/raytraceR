189edcb81090d763905a7b405f290a702d4043e5
dkilfoyle <dkilfoyle@xtra.co.nz>
2017-01-15 00:12:05 +1300

    red circle

---
+++ raytraceR.R
@@ -0,0 +1,66 @@
+library(magrittr)
+library(ggplot2)
+
+createBMP = function(width=200, height=100) {
+  return(list(
+    width=width,
+    height=height,
+    pixels=matrix(0, nrow=width*height, ncol=3, dimnames=list(NULL, c("R","G","B")))
+  ))
+}
+
+bmp.setPixel = function(bmp, x, y, color) {
+  bmp$pixels[(y-1)*bmp$width+x,] = color
+  return(bmp)
+}
+
+bmp.plot = function(bmp) {
+  z=as.data.frame(bmp$pixels)
+  z$y = rep(1:bmp$height, each=bmp$width)
+  z$x = rep(1:bmp$width, times=bmp$height)
+  
+    p = ggplot(data=z, aes(x=x, y=y, fill=rgb(R,G,B))) +
+      scale_fill_identity() +
+      geom_tile(show.legend=F) +
+      labs(x=NULL,y=NULL) +
+      coord_fixed() +
+      theme_minimal()
+
+    print(p)
+}
+
+createRay = function(origin = c(0,0,0), direction = c(1,0,0))
+{
+  return(list(
+    origin=origin,
+    direction=direction
+  ))
+}
+
+hit.sphere = function(center, radius, ray) {
+  oc = ray$origin - center
+  a = ray$direction %*% ray$direction
+  b= 2.0 * (oc %*% ray$direction)
+  c = (oc %*% oc) - radius * radius
+  discriminant = b * b - 4 * a* c
+  return(discriminant>0)
+}
+
+tst = function() {
+  lower.left.corner = c(-2,-1,-1)
+  horizontal=c(4,0,0)
+  vertical=c(0,2,0)
+  origin=c(0,0,0)
+  bmp = createBMP()
+  for (j in 1:bmp$height) {
+    for (i in 1:bmp$width) {
+      u = i/bmp$width
+      v = j/bmp$height
+      ray = createRay(origin=origin, direction=(lower.left.corner + (u*horizontal) + (v*vertical)))
+      if (hit.sphere(center=c(0,0,-1), radius=0.5, ray)) {
+        bmp$pixels[(j-1)*bmp$width+i,]=c(1,0,0)
+      }
+    }
+  }
+  bmp.plot(bmp)
+}
