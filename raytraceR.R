71361401b41d24ea4810e7ab66153cb813d54196
dkilfoyle <dkilfoyle@xtra.co.nz>
2017-01-15 20:40:02 +1300

    convert to R6 classes

--- raytraceR.R
+++ raytraceR.R
@@ -1,77 +1,122 @@
-library(magrittr)
+library(R6)
+library(assertthat)
 library(ggplot2)
 
-createBMP = function(width=200, height=100) {
-  return(list(
-    width=width,
-    height=height,
-    pixels=matrix(0, nrow=width*height, ncol=3, dimnames=list(NULL, c("R","G","B")))
+Bitmap = R6Class("Bitmap",
+  public = list(
+    width = NULL,
+    height = NULL,
+    pixels = NULL,
+    initialize = function(width=200, height=100) {
+      self$width=width
+      self$height=height
+      self$cls()
+    },
+    cls = function() {
+      self$pixels=matrix(0, nrow=self$width*self$height, ncol=3, 
+        dimnames=list(NULL, c("R","G","B")))
+    },
+    setPixel = function(x, y, color) {
+      assert_that(x <= self$width)
+      assert_that(y <= self$height)
+      self$pixels[(y-1)*self$width+x, ] = color
+    },
+    plot = function() {
+      z=as.data.frame(self$pixels)
+      z$y = rep(1:self$height, each=self$width)
+      z$x = rep(1:self$width, times=self$height)
+      
+      p = ggplot(data=z, aes(x=x, y=y, fill=rgb(R,G,B))) +
+        scale_fill_identity() +
+        geom_tile(show.legend=F) +
+        labs(x=NULL,y=NULL) +
+        coord_fixed() +
+        theme_minimal()
+      
+      print(p)
+    }
+  )
+)
+
+Ray = R6Class("Ray",
+  public = list(
+    origin = NULL,
+    direction = NULL,
+    initialize = function(origin = NA, direction = NA) {
+      self$origin = origin
+      self$direction = direction
+    },
+    getLength = function() { return( sqrt(self$direction %*% self$direction)) }
+  )
+)
+
+Sphere = R6Class("Sphere",
+  public = list(
+    center = NULL,
+    radius = NULL,
+    initialize = function(center = c(0,0,0), radius=1.0) {
+      self$center = center
+      self$radius = radius
+    },
+    intersects_ray = function(ray) {
+      oc = ray$origin - self$center
+      a = ray$direction %*% ray$direction # x^2 + y^2 + z^2
+      b= 2.0 * (oc %*% ray$direction)
+      c = (oc %*% oc) - self$radius * self$radius
+      discriminant = b * b - 4 * a* c
+      return(discriminant>0)
+    }
   ))
-}
 
-bmp.setPixel = function(bmp, x, y, color) {
-  bmp$pixels[(y-1)*bmp$width+x,] = color
-  return(bmp)
-}
-
-bmp.plot = function(bmp) {
-  z=as.data.frame(bmp$pixels)
-  z$y = rep(1:bmp$height, each=bmp$width)
-  z$x = rep(1:bmp$width, times=bmp$height)
-  
-    p = ggplot(data=z, aes(x=x, y=y, fill=rgb(R,G,B))) +
-      scale_fill_identity() +
-      geom_tile(show.legend=F) +
-      labs(x=NULL,y=NULL) +
-      coord_fixed() +
-      theme_minimal()
-
-    print(p)
-}
-
-createRay = function(origin = c(0,0,0), direction = c(1,0,0)) {
-  return(list(
-    origin=origin,
-    direction=direction
-  ))
-}
-
-ray.length = function(ray) {
-  return(sqrt(ray$direction %*% ray$direction))
-}
-
-hit.sphere = function(center, radius, ray) {
-  oc = ray$origin - center
-  a = ray$direction %*% ray$direction # x^2 + y^2 + z^2
-  b= 2.0 * (oc %*% ray$direction)
-  c = (oc %*% oc) - radius * radius
-  discriminant = b * b - 4 * a* c
-  return(discriminant>0)
-}
-
-getBackgroundColor = function(ray) {
-  unit.direction = ray$direction / ray.length(ray)
-  t = 0.5 * (unit.direction[2] + 1.0)
-  return( (1.0-t) * c(1,1,1) + (t*c(0.5,0.7,1.0)) )
-}
+Camera = R6Class("Camera",
+  public = list(
+    lower_left_corner = NULL,
+    horizontal = NULL,
+    vertical = NULL,
+    origin = NULL,
+    initialize = function(origin=c(0,0,0), lower_left_corner=c(-2,-1,-1), horizontal=c(4,0,0), vertical=c(0,2,0)) {
+      self$origin = origin
+      self$horizontal = horizontal
+      self$vertical = vertical
+      self$lower_left_corner = lower_left_corner
+    },
+    getEyeRay = function(u,v) {
+      assert_that(u <= 1 & v <= 1)
+      return(Ray$new(
+        origin=self$origin,
+        direction = self$lower_left_corner + (u*self$horizontal) + (v*self$vertical)
+      ))
+    },
+    getBackgroundColor = function(ray) {
+      unit.direction = ray$direction / ray$getLength()
+      t = 0.5 * (unit.direction[2] + 1.0)
+      return( (1.0-t) * c(1,1,1) + (t*c(0.5,0.7,1.0)) )
+    }
+  )
+)
 
 tst = function() {
-  lower.left.corner = c(-2,-1,-1)
-  horizontal=c(4,0,0)
-  vertical=c(0,2,0)
-  origin=c(0,0,0)
-  bmp = createBMP()
+  camera = Camera$new(
+    origin = c(0,0,0),
+    lower_left_corner = c(-2,-1,-1),
+    horizontal = c(4,0,0),
+    vertical=c(0,2,0))
+  
+  bmp = Bitmap$new(width=200, height=100)
+  sphere = Sphere$new(center=c(0,0,-1), radius=0.5)
+
   for (j in 1:bmp$height) {
     for (i in 1:bmp$width) {
       u = i/bmp$width
       v = j/bmp$height
-      ray = createRay(origin=origin, direction=(lower.left.corner + (u*horizontal) + (v*vertical)))
-      if (hit.sphere(center=c(0,0,-1), radius=0.5, ray)) {
-        bmp$pixels[(j-1)*bmp$width+i,]=c(1,0,0)
+      ray = camera$getEyeRay(u, v)
+      if (sphere$intersects_ray(ray)) {
+        bmp$setPixel(i,j,c(1,0,0))
       }
       else
-        bmp$pixels[(j-1)*bmp$width+i,]=getBackgroundColor(ray)
+        bmp$setPixel(i,j, camera$getBackgroundColor(ray))
     }
   }
-  bmp.plot(bmp)
+  
+  bmp$plot()
 }
